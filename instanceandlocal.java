efd2c3d4d39963c9d1bb20b80ea0dac54ea8f7fa
BXTPL9514M <mohammadlaxmidhar124@gmail.com>
2025-07-30 13:55:00 +0530

    Update instanceandlocal.java

--- instanceandlocal.java
+++ instanceandlocal.java
@@ -1,10 +1,9 @@
 public class instanceandlocal{
     int a=5;//instance variable
     public static void main(String[] args) {
-        int b=20;
-       // System.out.println(a*b);
-       variable v =new variable();
-        System.out.println((v . a *b));
-    }
-
+        int b=20;//local variable
+       System.out.println(b);//accessing local variable
+         instanceandlocal v =new v instanceandlocal(); //creating object for instance variable
+        System.out.println(v . a);//accessing instance variable through object
+          }
     }
